58bcae480a55eaecf4822cb6660632e47a9aa97b
Adejinmi Oluwapelumi <57303038+John-Atlas@users.noreply.github.com>
2020-08-09 23:51:56 +0100

    Create expo.rb

---
+++ expo.rb
@@ -0,0 +1,8 @@
+name = "Adejinmi"
+email = "oluwapelumijohnadejinmi@gmail.com"
+proglanguage = "ruby"
+biostack = "Genomics"
+slusername = "John"
+print <<PEL
+#{name}#{email}#{proglanguage}#{biostack}@#{slusername}
+PEL
