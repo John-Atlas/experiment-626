344ba158de430953a936d9403f51df5774146358
Adejinmi Oluwapelumi <57303038+John-Atlas@users.noreply.github.com>
2020-08-10 00:00:20 +0100

    Create helinski.rb

---
+++ helinski.rb
@@ -0,0 +1,9 @@
+# written on ruby version 1.4.0
+name = "Ade"
+email = "oluwapelumijohnadejinmi@gmail.com"
+proglanguage = "rubyro"
+biostack = "Genome"
+slusername = "Johnsky"
+print <<PEL
+#{name}#{email}#{proglanguage}#{biostack}@#{slusername}
+PEL
